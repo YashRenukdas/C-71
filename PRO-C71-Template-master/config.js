93d604d554de3ce50b360c5f6ea497e924f3fe76
YashRenukdas <90035728+YashRenukdas@users.noreply.github.com>
2022-03-18 14:04:47 +0530

    Add files via upload

---
+++ PRO-C71-Template-master/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyBCnIJc_4wUaIgORVKdyXAEpLinx7aZdv8",
+    authDomain: "e-ride-4cb52.firebaseapp.com",
+    projectId: "e-ride-4cb52",
+    storageBucket: "e-ride-4cb52.appspot.com",
+    messagingSenderId: "979953080553",
+    appId: "1:979953080553:web:f13dca955def191e43b878"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
